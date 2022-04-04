7087d48c35cb80e6f477a7ec022bb5baa5b3e3ba
Murilo Moraes Cabral <95389598+murilomcabral@users.noreply.github.com>
2022-04-04 14:41:20 -0300

    scrollbar teste

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.querySelector("#nAbout").onmouseover = function() {mouseOver()};
-document.querySelector("#nAbout").onmouseout = function() {mouseOut()};
-
-   function mouseOver() {
-      document.querySelector("#nAbout").style.color = "white";
-   }
-   function mouseOut() {
-      document.querySelector("#nAbout").style.color = "#aac0df";
-   }
+function hideScrollbar() {
+   
+}

5bdcf2664b80a02787aee5f1c01375927feb9374
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-04-05 22:52:30 -0300

    test

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function hideScrollbar() {
-   
+let controler = document.querySelector('#scrollbarbutton>strong').innerHTML
+
+if (controler == "On") {
+   document.querySelector("button#scrollbarbutton").style.background = "green";
+   document.querySelector("button#scrollbarbutton").style.color = "white";
+} else {
+   document.querySelector("button#scrollbarbutton").style.background = "#d00";
+   document.querySelector("button#scrollbarbutton").style.color = "white";
 }

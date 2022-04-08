471b7ef387505fccdd772d6ecc420ca0b965a2b7
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-04-08 00:21:26 -0300

    scrollbar teste

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let controler = document.querySelector('#scrollbarbutton>strong').innerHTML
+document.querySelector("button#scrollbarbutton").style.background = "green";
+document.querySelector("button#scrollbarbutton").style.color = "white";
 
-if (controler == "On") {
-   document.querySelector("button#scrollbarbutton").style.background = "green";
-   document.querySelector("button#scrollbarbutton").style.color = "white";
-} else {
-   document.querySelector("button#scrollbarbutton").style.background = "#d00";
-   document.querySelector("button#scrollbarbutton").style.color = "white";
+function scrollButton() {
+   if (controler != "On") {
+      document.querySelector("button#scrollbarbutton").style.background = "green";
+      document.querySelector("button#scrollbarbutton").style.color = "white";
+      document.querySelector("#scrollbarbutton>strong").innerHTML = "On";
+      controler = "On";
+      document.body.style.overflow = "visible";
+   } else {
+      document.querySelector("button#scrollbarbutton").style.background = "#d00";
+      document.querySelector("button#scrollbarbutton").style.color = "white";
+      document.querySelector("#scrollbarbutton>strong").innerHTML = "Off";
+      controler = "Off";
+      document.body.style.overflow = "?";
+   }
 }

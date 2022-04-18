397e2056507ca2729865d767271272d74fe65aad
Murilo Moraes Cabral <95389598+murilomcabral@users.noreply.github.com>
2022-04-18 13:29:44 -0300

    contando com javascript

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -7,25 +7,27 @@ function contarNum() {
    if (ini.value.length == 0 || fim.value.length == 0 || stp.value.length == 0) {
       alert('[ERRO 0x01] Faltam dados!')
    } else {
-      res.innerHTML = 'Contando:'
+      res.innerHTML = 'Contando: <br>'
       let i = Number(ini.value)
       let f = Number(fim.value)
       let p = Number(stp.value)
-
-      for(let c = i; c <= f; c += p) {
-         res.innerHTML += `${c} `
+      //caso o passo seja igual a zero
+      if (p <= 0) {
+         window.alert ('Passo inválido! Considerando PASSO 1')
+         p = 1
       }
+      if (i < f) {
+         //contagem crescente
+         for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449} `
+         }
+      } else {
+         //contagem decrescente
+         for(let c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} \u{1F449} `
+         }
+      }
+      //fim da contagem
+      res.innerHTML += `\u{1F3C1}`
    }
 }
-
-// if (ini || fin == ('')) {
-//    finalMsg = 'Digite valores'
-// } else if (ini < fin) {
-//    while (ini <= fin) {
-//       ini += stp
-//    }
-// } else {
-//    while (ini >= fin) {
-//       ini -= stp
-//    }
-// }  

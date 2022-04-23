580e4fc5f753f70962f7ac1b1cd431e3bbd48b25
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-04-23 15:54:05 -0300

    ..

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,6 +1,7 @@
 function tabuada(){
    let num = document.getElementById('txtn')
    let tab = document.getElementById('selTab')
+   let rep = 10
    // attrSize.setAttribute("size", "10");
    
    if (num.value.length == 0) {
@@ -10,14 +11,24 @@ function tabuada(){
       let c = 1
       tab.innerHTML = ''
       let attrSize = document.querySelector('#selTab')
-      attrSize.setAttribute('size','10')
+      attrSize.setAttribute('size', rep)
       
-      while (c <= 10) {
+      do {
          let item = document.createElement('option')
          item.text = `${n} x ${c} = ${n*c}`
          tab.value = `tab${c}`
+         // tab.value é necessário para integração com Python, mas não tem influência direta no código
          tab.appendChild(item)
          c++
-      }
+      } while ( c <= rep)
+
+
+      // while (c <= rep) {
+      //    let item = document.createElement('option')
+      //    item.text = `${n} x ${c} = ${n*c}`
+      //    tab.value = `tab${c}`
+      //    tab.appendChild(item)
+      //    c++
+      // }
    }
 }
--- files/script.js
+++ files/script.js
@@ -8,14 +8,12 @@ function scrollButton() {
       document.querySelector("button#scrollbarbutton").style.color = "white";
       document.querySelector("#scrollbarbutton>strong").innerHTML = "On";
       controler = "On";
-      document.setAttribute('','')
-      // atenção ao setAttribute
+      // esconder barra de rolagem (scrollbar)
    } else {
       document.querySelector("button#scrollbarbutton").style.background = "#d00";
       document.querySelector("button#scrollbarbutton").style.color = "white";
       document.querySelector("#scrollbarbutton>strong").innerHTML = "Off";
       controler = "Off";
-      document.setAttribute('','')
-      // atenção ao setAttribute
+      // esconder barra de rolagem (scrollbar)
    }
 }

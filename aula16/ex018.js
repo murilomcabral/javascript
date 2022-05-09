8946b1fd5c5edbf447ded53526d4527aab0d4bae
Murilo Moraes Cabral <95389598+murilomcabral@users.noreply.github.com>
2022-05-09 14:41:24 -0300

    ex

--- aula16/ex018.js
+++ aula16/ex018.js
@@ -1,17 +1,17 @@
 let c = 0 //determina o objeto no array
+let lista = []
 
-function addValor(){
+function addValor() {
    let num = Number(document.getElementById('numInput').value)
    let box = document.getElementById('inputBox')
    box.innerHTML = ""
    if (num < 1 || num > 100) {
       window.alert('Por favor, digite um número válido!')
    } else {
-      
+      lista = num[c]
       let item = document.createElement('option')
       item.text = (`O valor ${num} foi adicionado.`)
       box.appendChild(item)
-
       c++ //adiciona 1 ao valor
 
       if (box.hasAttribute('size') < c) {
@@ -19,5 +19,13 @@ function addValor(){
       }
    }
    
+}
+
+function analise() {
+   if (box = "") {
+      window.alert('Não há dados para serem analisados.')
+   } else {
+      window.alert('Lista')
+   }
 }
 //array e append em section

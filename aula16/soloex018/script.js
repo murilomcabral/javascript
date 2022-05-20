6712231278e08599a0b5b4433838a1e29e10e077
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-05-20 13:31:34 -0300

    finalizando vetor

--- aula16/soloex018/script.js
+++ aula16/soloex018/script.js
@@ -3,15 +3,33 @@ let lista = document.querySelector('select#listBox')
 let res = document.querySelector('div#res')
 let valores = []
 
+function isNumber(n) {
+   if (n >= 1 && n <= 100) {
+      return true
+   } else {
+      return false
+   }
+}
+
+function inList(n, l) {
+   if (l.indexOf(Number(n)) != -1) {
+      return true
+   } else {
+      return false
+   }
+}
 
 function validar() {
-   if (num.value < 1 || num.value > 100) {
-      window.alert(`Por favor, digite um número válido! Número \"${num.value}\" inválido.`)
-      num.focus()
-   } else {
+   if (isNumber(num.value) && !inList(num.value, valores)) {
+      valores.push(Number(num.value))
       let item = document.createElement('option')
-      item.text = `Número ${num.value} adicionado a lista.`
+      item.text = `Valor ${num.value} adicionado a lista.`
       lista.appendChild(item)
+      res.innerHTML = ''
+   } else {
+      window.alert(`Número inválido ou já consta na lista. \[${num.value}\]`)
+      num.value = ''
+      num.focus()
    }
    //window.alert(Number(num.value))
 }

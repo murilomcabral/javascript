9cdaaa74c81b22cb0c8ed5b8ee7ef74fe676d7b9
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-05-22 01:30:44 -0300

    exercicio finalizado
    ainda treinando loops após o exerc final

--- aula16/soloex018/script.js
+++ aula16/soloex018/script.js
@@ -26,10 +26,45 @@ function validar() {
       item.text = `Valor ${num.value} adicionado a lista.`
       lista.appendChild(item)
       res.innerHTML = ''
+      num.value = ''
+      num.focus()
    } else {
       window.alert(`Número inválido ou já consta na lista. \[${num.value}\]`)
       num.value = ''
       num.focus()
    }
-   //window.alert(Number(num.value))
+}
+
+function analisar() {
+   if (valores.length != 0) {
+
+      let tot = valores.length
+      let sum = 0
+      let max = valores[0]
+      let min = valores[0]
+
+      for (let pos in valores) {
+         if (valores[pos] > max) {
+            max = valores[pos]
+         } else if (valores[pos] < min) {
+            min = valores[pos]
+         }
+      }
+
+      for (let pos in valores) {
+         sum += valores[pos]
+      }
+
+      let med = sum / tot
+
+      res.innerHTML = ''
+      res.innerHTML += `<p>No total foram ${tot} números cadastrados.</p>`
+      res.innerHTML += `<p>A soma dos valores é ${sum}.</p>`
+      res.innerHTML += `<p>A média dos valores é ${med.toFixed(2)}.</p>`
+      res.innerHTML += `<p>O maior valor validado foi ${max}.</p>`
+      res.innerHTML += `<p>O menor valor validado foi ${min}.</p>`
+
+   } else {
+      window.alert('Não há dados')
+   }
 }

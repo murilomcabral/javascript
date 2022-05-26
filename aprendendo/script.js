b535db4be535ae22c9b8500d667c5d29186385b8
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-05-26 20:34:42 -0300

    loops
    entendendo melhor loops e como trabalhar com eles

--- aprendendo/script.js
+++ aprendendo/script.js
@@ -1,9 +1,18 @@
 let cNota = document.getElementById('contaNota')
 let nota = document.querySelector('input#nota')
 let bNotas = []
+let n = 1
 
 function cadastrar() {
-   for(let n = 1; n <= 4; n++) {
-      cNota.textContent = `${n}`
-   }
+   do {
+         if (nota.value >= 0 && nota.value <= 10) {
+         n++
+         cNota.textContent = `${n}`
+         bNotas.push(nota.value)      
+      } else {
+         window.alert(`Digite um valor válido.[${nota.value} inválido]`)
+      }
+      nota.value = ''
+      nota.focus()
+   } while (bNotas.length < 4) //corrigir código - o objetivo seria limitar a quantidade de notas cadastradas a 4 notas e mostrar o resultado após a validação da quarta nota.
 }

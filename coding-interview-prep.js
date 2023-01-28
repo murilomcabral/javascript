092caf5841725dcc68d5c7bb3ce194db3390dbb3
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-01-28 01:06:04 -0300

    t

--- coding-interview-prep.js
+++ coding-interview-prep.js
@@ -1,35 +1,51 @@
-// https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-1-multiples-of-3-and-5
+// // https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-1-multiples-of-3-and-5
 
-function multiplesOf3and5(number) {
-  let sum = 0
-  for (let i = 0; i < number; i++) {
-    if (i%3 === 0 || i%5 === 0) {
-      sum += i
-    }
+// function multiplesOf3and5(number) {
+//   let sum = 0
+//   for (let i = 0; i < number; i++) {
+//     if (i%3 === 0 || i%5 === 0) {
+//       sum += i
+//     }
+//   }
+//   return sum;
+// }
+
+// multiplesOf3and5(1000);
+
+// //https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-2-even-fibonacci-numbers
+
+// function fiboEvenSum(n) {
+//   const arr = []
+//   let pri = 0
+//   let sec = 1
+//   let mid
+//   for (let i = 0; sec < n; i++) {
+//     mid = pri + sec
+//     pri = sec
+//     sec = mid
+//     arr.push(sec)
+//   }
+//   let sum = 0
+//   for (let n of arr) {
+//     if (n%2 === 0) {
+//       sum += n;
+//     }
+//   }
+//   return sum
+// }
+
+//https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-3-largest-prime-factor
+
+function largestPrimeFactor(number) {
+  let prime = 2,
+  max = 2;
+  while (prime <= number) {
+    if (number % prime == 0) {
+      max = prime;
+      number = number / prime;
+    } else prime++; //Only increment the prime number if the number isn't divisible by it
   }
-  return sum;
+  return max;
 }
 
-multiplesOf3and5(1000);
-
-//https://www.freecodecamp.org/learn/coding-interview-prep/project-euler/problem-2-even-fibonacci-numbers
-
-function fiboEvenSum(n) {
-  const arr = []
-  let pri = 0
-  let sec = 1
-  let mid
-  for (let i = 0; sec < n; i++) {
-    mid = pri + sec
-    pri = sec
-    sec = mid
-    arr.push(sec)
-  }
-  let sum = 0
-  for (let n of arr) {
-    if (n%2 === 0) {
-      sum += n;
-    }
-  }
-  return sum
-}
+console.log(largestPrimeFactor(8))

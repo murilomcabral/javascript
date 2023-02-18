27b1c4ed309db4b4d47d424647e9606c78e4d8fe
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-02-18 11:07:44 -0300

    Update coding-interview-prep.js

--- coding-interview-prep.js
+++ coding-interview-prep.js
@@ -2,15 +2,30 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 //Spread
 
-const array1 = [1, 2, 3]
-const array2 = [5, 2, 1, 4]
+// const array1 = [1, 2, 3]
+// const array2 = [5, 2, 1, 4]
 
-const diff = (arr1, arr2) => [
-   ...arr1.filter(e => !arr2.includes(e)),
-   ...arr2.filter(e => !arr1.includes(e))
- ];
+// const diff = (arr1, arr2) => [
+//    ...arr1.filter(e => !arr2.includes(e)),
+//    ...arr2.filter(e => !arr1.includes(e))
+//  ];
  
- const sym = (...args) => [...new Set(args.reduce(diff))];
+//  const sym = (...args) => [...new Set(args.reduce(diff))];
+
+function multiplesOf3and5(number) {
+   let arrNum = []
+   for (let i = 1; i < number; i++) {
+     if (i % 3 === 0 || i % 5 === 0) {
+       arrNum.push(i)
+     }
+   }
+      let initial = 0
+      let res = arrNum.reduce((accumulator, current) => accumulator + current, initial);
+      return res
+ }
+ 
+ console.log(multiplesOf3and5(10));
+
 
 // const col = document.querySelector('button[type=submit]')
 // col.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()

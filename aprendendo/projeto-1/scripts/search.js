7a42e01e6f560f2aa2cdbed4916be17d29f94550
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-02-22 00:54:54 -0300

    progress

--- aprendendo/projeto-1/scripts/search.js
+++ aprendendo/projeto-1/scripts/search.js
@@ -1,9 +1,11 @@
-const searchInput = document.querySelector('.search-input')
+const searchInput = document.querySelector('[search-input]')
 
 function showSearchInput() {
    if (searchInput.style.display === 'none') {
       searchInput.style.display = 'inline-block'
+      searchInput.focus()
    } else {
       searchInput.style.display = 'none'
+      searchInput.value = ''
    }
 }

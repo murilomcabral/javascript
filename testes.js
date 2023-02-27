f9844b33778729bec5835533ad8c1d918838cd90
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-02-27 13:37:40 -0300

    spread

--- testes.js
+++ testes.js
@@ -1,35 +1,52 @@
-function largestPrimeFactor(number) {
-   let prim = 2
-   while (number/prim > 1) {
-      if(number % prim === 0) {
-         number /= prim
-      } else {prim++}
-   }
-   return prim;
- }
+const arr1 = [3,4]
 
- //#region Teste
+function sum(a,b,c,d) {
+   return Math.sqrt((a**2)+(b**2))
+}
+console.log(sum(...arr1))
 
- const tests = [
-   {'entryNum': 2,'returnValue': 2},
-   {'entryNum': 3,'returnValue': 3},
-   {'entryNum': 5,'returnValue': 5},
-   {'entryNum': 7,'returnValue': 7},
-   {'entryNum': 8,'returnValue': 2},
-   {'entryNum': 13195,'returnValue': 29},
-   {'entryNum': 600851475143,'returnValue': 6857},
- ]
+// const diff = (arr1, arr2) => [
+//    ...arr1.filter(e => !arr2.includes(e)),
+//    ...arr2.filter(e => !arr1.includes(e))
+//  ];
+ 
+//  const sym = (...args) => [...new Set(args.reduce(diff))];
+ 
+//  // test here
+// console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+ 
+// function largestPrimeFactor(number) {
+//    let prim = 2
+//    while (number/prim > 1) {
+//       if(number % prim === 0) {
+//          number /= prim
+//       } else {prim++}
+//    }
+//    return prim;
+//  }
 
- function testerMachine() {
-   let n = 0
-   let r = 0
-   for (let i = 0; i < tests.length; i++) {
-      n = tests[i].entryNum
-      r = tests[i].returnValue
-      console.log(largestPrimeFactor(n) === r ? `largestPrimeFactor(${n}) OK!` : `largestPrimeFactor(${n}) should return ${r}`)
-   }
- }
+//  //#region Teste
 
-  testerMachine()
+//  const tests = [
+//    {'entryNum': 2,'returnValue': 2},
+//    {'entryNum': 3,'returnValue': 3},
+//    {'entryNum': 5,'returnValue': 5},
+//    {'entryNum': 7,'returnValue': 7},
+//    {'entryNum': 8,'returnValue': 2},
+//    {'entryNum': 13195,'returnValue': 29},
+//    {'entryNum': 600851475143,'returnValue': 6857},
+//  ]
 
-  //#endregion 
+//  function testerMachine() {
+//    let n = 0
+//    let r = 0
+//    for (let i = 0; i < tests.length; i++) {
+//       n = tests[i].entryNum
+//       r = tests[i].returnValue
+//       console.log(largestPrimeFactor(n) === r ? `largestPrimeFactor(${n}) OK!` : `largestPrimeFactor(${n}) should return ${r}`)
+//    }
+//  }
+
+//   testerMachine()
+
+//   //#endregion 

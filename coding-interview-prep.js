a999d79ff3100c462e33ad7f6b2d5f33d2cf1434
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-06-18 01:23:37 -0300

    del

--- coding-interview-prep.js
+++ coding-interview-prep.js
@@ -2,29 +2,18 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 //Spread
 
-// const array1 = [1, 2, 3]
-// const array2 = [5, 2, 1, 4]
+const diff = (arr1, arr2) => [
+  ...arr1.filter(e => !arr2.includes(e)),
+  ...arr2.filter(e => !arr1.includes(e))
+];
 
-// const diff = (arr1, arr2) => [
-//    ...arr1.filter(e => !arr2.includes(e)),
-//    ...arr2.filter(e => !arr1.includes(e))
-//  ];
- 
-//  const sym = (...args) => [...new Set(args.reduce(diff))];
+const sym = (...args) => [...new Set(args.reduce(diff))];
 
-function multiplesOf3and5(number) {
-   let arrNum = []
-   for (let i = 1; i < number; i++) {
-     if (i % 3 === 0 || i % 5 === 0) {
-       arrNum.push(i)
-     }
-   }
-   let res = arrNum.reduce((accumulator, current) => accumulator + current, 0);
-   return res
- }
-
- console.log(multiplesOf3and5(5))
-
+// test here
+console.log('=========================')
+console.log('Spread operator:')
+console.log(sym([1, 2, 3], [5, 2, 1, 4]))
+console.log('=========================')
 
 // const col = document.querySelector('button[type=submit]')
 // col.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
